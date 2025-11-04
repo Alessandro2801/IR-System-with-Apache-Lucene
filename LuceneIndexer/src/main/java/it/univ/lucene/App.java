753db2bbfcea44c8290604e7925929836d3bc0ae
cavins01 <128725433+Alessandro2801@users.noreply.github.com>
2025-11-04 11:25:52 +0100

    Update App.java

--- LuceneIndexer/src/main/java/it/univ/lucene/App.java
+++ LuceneIndexer/src/main/java/it/univ/lucene/App.java
@@ -2,8 +2,8 @@ package it.univ.lucene;
 
 public class App {
     public static void main(String[] args) {
-        String docsPath = "dataset/files";
-        String indexPath = "index";
+        String docsPath = "../dataset/files";
+        String indexPath = "../index";
         long start = System.currentTimeMillis();
         Indexer.createIndex(docsPath, indexPath);
         long end = System.currentTimeMillis();
